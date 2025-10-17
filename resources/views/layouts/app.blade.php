ff5793ff35059acab3a17a595b3e607c9d2cd76d
sdrenjith <sd.renjithreghu@gmail.com>
2025-10-17 18:17:07 +0530

    iphone view fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="google-site-verification" content="tQEHX8VUt_rcCaxLkD6xl_pUzDFhPfyqeJFtpfu-xU8" />
     <meta name="google-site-verification" content="JPrLU44c0wy90nU6w5dzciHXTduGuLWHUC2S21j7fJU" />
